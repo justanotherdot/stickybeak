24f220835701b0269c2f87ea4a85fb372093f91a
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-01-03 08:41:01 +1100

    Switch over to event chan approach for triggers

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
--- import           Config             (Config (triggers), parseConfig)
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as T
--- import           System.Exit        (ExitCode (ExitFailure), exitWith)
-import           WatchUtils         (subscribe)
-
-import           Control.Concurrent
-import           Control.Monad      (forever)
-import           System.INotify
+import           Control.Monad  (void)
+import           System.INotify (EventVariety (..), removeWatch)
+import           WatchUtils     (runCmd, subscribe, withEventChan)
 
 main :: IO ()
 main = do
-  (eventChan, wd) <- subscribe [Open] "src"
-  _ <- forkIO $ forever (readChan eventChan >>= \evt -> T.putStrLn (T.pack (show evt)))
-  _ <- getLine
+  (eventChan, wd) <- subscribe [Modify] "src"
+  withEventChan eventChan (runCmd "echo" ["hello"] ".")
+  void getLine
   removeWatch wd
-
--- testTriggers :: IO ()
--- testTriggers = do
---   config <- parseConfig "stickybeak.yaml"
-  -- case config of
-  --   Just config' -> mapM_ setupTrigger (triggers config')
-  --   Nothing      -> do
-  --     T.putStrLn "Invalid config file provided"
-  --     exitWith $ ExitFailure 1
--- src/WatchUtils.hs
+++ src/WatchUtils.hs
@@ -1,58 +1,32 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-
 module WatchUtils
-  ( setupTrigger
-  , subscribe
+  ( subscribe
+  , runCmd
+  , withEventChan
   ) where
 
-import           Config               (TriggerItem (..))
-import qualified Control.Exception    as E
-import qualified Data.Text            as T
--- import qualified Data.Text.IO         as T
-import           System.Exit          (ExitCode (..))
-import           System.Posix.Signals
-import           System.Process       (CreateProcess (..), createProcess, proc)
-
 import           Control.Concurrent
-import           Control.Monad        (forever)
+import           Control.Monad      (forever, void)
 import           System.INotify
+import           System.Process     (CreateProcess (..), createProcess, proc)
 
+-- | Subscribe to events, as described by passed eventTypes, on path.
 subscribe :: [EventVariety] -> FilePath -> IO (Chan Event, WatchDescriptor)
-subscribe _eventTypes path = do
+subscribe eventTypes path = do
   inotify <- initINotify
   eventChan <- newChan
-  -- print inotify
   wd <- addWatch
           inotify
-          [Modify]
+          eventTypes
           path
           (writeChan eventChan)
-  -- print wd
   putStrLn "Listens to your home directory. Hit enter to terminate."
   return (eventChan, wd)
-
--- TODO replace this ctrl-c business with daemonization.
--- | Handle SigInt from Ctrl-C on *nix systems to break out of the loop.
---   By default, loops at every second (1e6 microseconds)
-handleCtrlC :: IO ()
-handleCtrlC = do
-  tid <- myThreadId
-  _ <- installHandler keyboardSignal (Catch (E.throwTo tid ExitSuccess)) Nothing
-  forever $ threadDelay 1000000
-
--- | Given a TriggerItem, setup a trigger provided it's specification.
-setupTrigger :: TriggerItem -> IO ()
-setupTrigger TriggerItem{..} = do
-  runCmd (T.unpack cmd) (fmap T.unpack args) path
-  handleCtrlC
-  where path = T.unpack $ head dirs
-  -- withManager $ \mgr -> do
-  --   when (name /= "") $ T.putStrLn ("Setting up '" `append` name `append` "'")
-  --   _stopSig <- watchDir mgr path (const True)
 
 -- | Run a command on the current shell instance.
 runCmd :: FilePath -> [FilePath] -> FilePath -> IO ()
 runCmd cmd args dir = do
   _ <- createProcess (proc cmd args){ cwd = Just dir}
   return ()
+
+withEventChan :: Chan Event -> IO () -> IO ()
+withEventChan chan cmd = void . forkIO . forever $ readChan chan >> cmd
