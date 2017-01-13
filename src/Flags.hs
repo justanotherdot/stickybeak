c1a406bf534a4663a2daa439dda50ea4ff4cd975
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-01-13 12:50:01 +1100

    Add recursive functionality to triggers
    This resolves #3 and #9.

--- src/Flags.hs
+++ src/Flags.hs
@@ -4,23 +4,24 @@ module Flags (SBMode(..), getCmdLine) where
 
 import           System.Console.CmdArgs
 
-data SBMode = Watch { dir :: Maybe FilePath
-                    , cmd :: Maybe FilePath
-                    , rec :: Bool
+data SBMode = Watch { dir       :: Maybe FilePath
+                    , cmd       :: Maybe FilePath
+                    , recursive :: Bool
                     }
-            | Triggers { config :: Maybe FilePath }
+            | Triggers { config    :: Maybe FilePath }
             deriving (Show, Data, Typeable)
 
 watch :: SBMode
 watch = Watch{ dir = def &= help "Directory to watch" &= typ "DIR"
              , cmd = def &= help "Command to run on file changes" &= typ "CMD"
-             , rec = def &= help "Watch subdirectories, as well."
+             , recursive = def &= help "Watch subdirectories, as well."
              } &= help "Watch dir and run cmd on file changes"
 
 triggers :: SBMode
 triggers = Triggers{ config = def
                            &= help "Config file specifying triggers to setup"
-                           &= typFile } &= help "Specify triggers from a config file"
+                           &= typFile
+                   } &= help "Specify triggers from a config file"
 
 cmdLineMode :: Mode (CmdArgs SBMode)
 cmdLineMode = cmdArgsMode $ modes [triggers &= auto, watch]
--- src/StickyBeak.hs
+++ src/StickyBeak.hs
@@ -1,29 +1,31 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module StickyBeak (stickybeak, subDirectories) where
+module StickyBeak (stickybeak) where
 
 import           Config           (Config (..), defaultConfig, parseConfig)
 import           Control.Monad    (unless, void, when)
 import           Data.Maybe       (fromMaybe, isNothing)
 import           Error            (exitFailureMsg)
 import           Flags
-import           System.Directory (doesDirectoryExist, doesFileExist,
-                                   listDirectory)
-import           System.FilePath  (takeBaseName, (</>))
+import           System.Directory (doesFileExist)
 import           System.INotify   (removeWatch)
-import           WatchUtils       (watch, watchFromTrigger)
+import           WatchUtils       (watch, watchFromTrigger, watchRec)
 
 stickybeak :: IO ()
 stickybeak = do
   mode <- getCmdLine
   case mode of
-    Watch{..}    -> watchMode dir cmd
+    Watch{..}    -> if recursive
+                      then watchModeRec dir cmd
+                      else watchMode dir cmd
     Triggers{..} -> triggerMode (fromMaybe defaultConfig config)
 
 waitToQuit :: IO ()
 waitToQuit = putStrLn "ctrl-c to quit" >> void getLine
 
-checkWatchModeArgs :: Maybe FilePath -> Maybe FilePath -> IO (FilePath, String, [String])
+checkWatchModeArgs :: Maybe FilePath
+                   -> Maybe FilePath
+                   -> IO (FilePath, String, [String])
 checkWatchModeArgs dir cmd = do
   cmd' <- case cmd of
             Nothing -> exitFailureMsg "Error: Did not provide a command to run"
@@ -33,26 +35,15 @@ checkWatchModeArgs dir cmd = do
 
 watchMode :: Maybe FilePath -> Maybe FilePath -> IO ()
 watchMode dir cmd = do
-  (dir', cmd', args) <- checkWatchModeArgs dir cmd
-  wd <- watch dir' cmd' args
+  (dir', cmd', cmdArgs') <- checkWatchModeArgs dir cmd
+  wd <- watch dir' cmd' cmdArgs'
   waitToQuit
   removeWatch wd
 
-subDirectories :: FilePath -> IO [FilePath]
-subDirectories dir = do
-  isDir <- doesDirectoryExist dir
-  if isDir
-    then do
-      es <- fmap (dir</>) <$> listDirectory dir
-      ds <- mapM subDirectories es
-      return (takeBaseName dir : concat ds)
-    else return []
-
 watchModeRec :: Maybe FilePath -> Maybe FilePath -> IO ()
 watchModeRec dir cmd = do
-    (dir', cmd', args) <- checkWatchModeArgs dir cmd
-    subDirs <- subDirectories dir'
-    wds <- traverse (\d -> watch d cmd' args) subDirs
+    (dir', cmd', cmdArgs') <- checkWatchModeArgs dir cmd
+    wds <- watchRec dir' cmd' cmdArgs'
     waitToQuit
     removeWatches wds
   where removeWatches = mapM_ removeWatch
@@ -64,7 +55,7 @@ triggerMode path = do
     conf <- parseConfig path
     when (isNothing conf) (exitFailureMsg $ "Error: Could not parse " ++ path)
     let Config{..} = fromMaybe Config{triggers = []} conf
-    wds <- mapM watchFromTrigger triggers
+    wds <- concat <$> mapM watchFromTrigger triggers
     waitToQuit
     removeWatches wds
-  where removeWatches = mapM_ (mapM_ removeWatch)
+  where removeWatches = mapM_ removeWatch
