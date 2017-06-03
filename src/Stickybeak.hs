46020e8ab5a88304cff4f1c1edd46aa1bb956726
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-06-03 17:22:28 +1000

    Do more experiments with freemonads by carving out API

--- src/Stickybeak.hs
+++ src/Stickybeak.hs
@@ -3,58 +3,64 @@
 
 module Stickybeak
   ( echo
+  , echo'
   , runIO
+  , runPure
   ) where
 
 {- import           Control.Concurrent.Chan.Unagi.Bounded (InChan, OutChan) -}
+{- import           System.INotify                        (INotify, WatchDescriptor) -}
 import           Control.Monad.Free
-import           System.Exit        (exitSuccess)
-{- import           System.INotify                        (INotify, -}
-                                                        {- WatchDescriptor) -}
+import           Data.Semigroup      ((<>))
+import           Options.Applicative
+import           System.Exit         (exitSuccess)
 
-{- data Job where -}
-  {- Job :: (InChan, OutChan) -> INotify -> Maybe WatchDescriptor -> Job  -}
+data Job = Job
+  { chans   :: (String, String)
+  , inotify :: String
+  , wd      :: String
+  } deriving (Show)
 
-{- data Job = Job -}
-  {- { channels :: (InChan, OutChan) -}
-  {- , inotify  :: INotify -}
-  {- , wd       :: Maybe WatchDescriptor -}
-  {- } -}
-
-{- data Job where -}
-  {- Job :: Job -}
-
-{- data Stickybeak n where -}
-  {- Subscribe :: Job -> FilePath -> (a -> IO ()) -> IO () -}
-
-data StickybeakF x where
-  {- CheckArgs   :: StickybeakF x -}
-  Subscribe   :: (String -> x) -> StickybeakF x
-  Unsubscribe :: String -> x -> StickybeakF x
-  ExitSuccess :: StickybeakF x
-  {- ExitFailure :: StickybeakF x -}
+data StickybeakF next where
+  CheckArgs   :: StickybeakF next
+  Exit        :: StickybeakF next
+  Subscribe   :: (Job -> next) -> StickybeakF next
+  Unsubscribe :: Job -> next -> StickybeakF next
   deriving (Functor)
 
 type Stickybeak = Free StickybeakF
 
-unsubscribe :: String -> Stickybeak ()
+unsubscribe :: Job -> Stickybeak ()
 unsubscribe s = liftF $ Unsubscribe s ()
 
-subscribe :: Stickybeak String
+subscribe :: Stickybeak Job
 subscribe = liftF $ Subscribe id
 
-exitSuccess' :: Stickybeak r
-exitSuccess' = liftF ExitSuccess
+exit :: Stickybeak r
+exit = liftF Exit
 
 runIO :: Stickybeak r -> IO r
+runIO (Free (Subscribe f))     = return (Job ("ic", "oc") "in" "wd") >>= runIO . f
+runIO (Free (Unsubscribe j n)) = print j >> runIO n
+runIO (Free CheckArgs)         = exitSuccess
+runIO (Free Exit)              = exitSuccess
 runIO (Pure r)                 = return r
-runIO (Free (Unsubscribe s n)) = putStrLn s >> runIO n
-runIO (Free (Subscribe f))     = getLine >>= runIO . f
-runIO (Free ExitSuccess)       = exitSuccess
+
+runPure :: Stickybeak r -> Maybe r
+runPure (Free (Subscribe n))     = return (Job ("ic", "oc") "in" "wd") >>= runPure . n
+runPure (Free (Unsubscribe _ n)) = runPure n
+runPure (Free CheckArgs)         = Nothing
+runPure (Free Exit)              = Nothing
+runPure (Pure r)                 = return r
 
 echo :: Stickybeak ()
 echo = do
-  str <- subscribe
-  unsubscribe str
-  _ <- exitSuccess'
-  unsubscribe str
+  job <- subscribe
+  unsubscribe job
+  _ <- exit
+  unsubscribe job -- Make sure it deos not print again.
+
+echo' :: Stickybeak Job
+echo' = do
+  job <- subscribe
+  return job
